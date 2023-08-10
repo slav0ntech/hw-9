a3c5b3eee1e3f90b7850156fab944998bb57239e
slav0ntech <masqueradeit@gmail.com>
2023-08-10 19:01:24 +0300

    add def handler_get_contact

--- main.py
+++ main.py
@@ -17,8 +17,10 @@ def handler_change_contact(contact):
             CONTACTS[key] = dict_result[key]
 
 
-def handler_get_contact():
-    pass
+def handler_get_contact(contact):
+    str_item_phone_remove = contact.replace('phone', '').strip()
+    if str_item_phone_remove in CONTACTS:
+        print(CONTACTS.get(str_item_phone_remove))
 
 
 def handler_show_all_contacts():
@@ -28,6 +30,7 @@ def handler_show_all_contacts():
 def main():
     pattern_add_conact = r'^[add]{3}\s{1}'
     pattern_change_conact = r'^[change]{6}\s{1}'
+    pattern_get_contact = r'[phone]{5}\s{1}'
 
     while True:
         result = input("waiting command -> ...\n")
@@ -35,7 +38,7 @@ def main():
             print(f'Good bye!')
             break
 
-        elif result.lower() in ("hello"):
+        elif result.lower() == "hello":
             print(f"How can I help you?")
 
         elif re.findall(pattern_add_conact, result):
@@ -44,7 +47,10 @@ def main():
         elif re.findall(pattern_change_conact, result):
             handler_change_contact(result)
 
-        elif result.lower() in ("show all"):
+        elif re.findall(pattern_get_contact, result):
+            handler_get_contact(result)
+
+        elif result.lower() == "show all":
             handler_show_all_contacts()
 
 
