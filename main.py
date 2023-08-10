72d910568d3ad632216de287c9383717182dacf2
slav0ntech <masqueradeit@gmail.com>
2023-08-10 12:21:08 +0300

    add handler_add_contact

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ import re
 CONTACTS = dict()  # {'name': 'tel number'}
 
 
-def handler_add_contact():
-    pass
+def handler_add_contact(contact: str):
+    item_add_remove = [x for x in contact.split(' ') if x != 'add']
+    for k, v in zip(['name', 'number'], item_add_remove):
+        CONTACTS[k] = v
+    print(f'{CONTACTS}')
 
 
 def handler_change_contact():
@@ -24,16 +27,16 @@ def main():
     pattern_change_conact = r'^[change]{6}\s{1}'
 
     while True:
-        result = input("Hi, what the command\n")
+        result = input("waitinig command -> ...\n")
         if result.lower() in ("good bye", "close", "exit"):
             print(f'Good bye!')
             break
 
-        elif result in ("hello"):
+        elif result.lower() in ("hello"):
             print(f"How can I help you?")
 
         elif re.findall(pattern_add_conact, result):
-            print('OK')
+            handler_add_contact(result)
 
 
 # def save_contacts(contacts: str):
