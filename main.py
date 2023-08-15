2d98d2c3273f426e5fe6c7b4a03f14af4491dab8
slav0ntech <masqueradeit@gmail.com>
2023-08-15 20:46:34 +0300

    fixed handllers

--- main.py
+++ main.py
@@ -3,65 +3,48 @@ import re
 CONTACTS = dict()
 
 
-def input_error(func):
-    def fixed_contacts(x):
-        result = func(x)
-        return result
-    return fixed_contacts
+def input_error(inner):
+    def wrap(*args):
+        try:
+            return inner(*args)
+        except IndexError:
+            return "Give me name and phone please"
+    return wrap
+
+
+def parser_command(command):
+    pattern_add_conact = r'^[aAdD]{3}\s{1}'
+    if re.findall(pattern_add_conact, command):
+        x = [x.lower() for x in command.split(' ') if x.lower() != 'add']
+        return handler_add_contact(x)
+
+    elif command.lower() == 'show all':
+        print(f'CONTACTS -> {CONTACTS}')
+        return handler_show_all_contacts()
 
 
 @input_error
 def handler_add_contact(contact: str):
-    list_item_add_remove = [x for x in contact.split(' ') if x != 'add']
-    CONTACTS[list_item_add_remove[0]] = list_item_add_remove[1]
-    return CONTACTS
-
-
-def handler_change_contact(contact: str):
-    dict_result = {}
-    list_item_change_remove = [x for x in contact.split(' ') if x != 'change']
-    dict_result[list_item_change_remove[0]] = list_item_change_remove[1]
-    for key in CONTACTS.keys():
-        if key in dict_result:
-            CONTACTS[key] = dict_result[key]
-
-
-def handler_get_contact(contact):
-    str_item_phone_remove = contact.replace('phone', '').strip()
-    if str_item_phone_remove in CONTACTS:
-        print(CONTACTS.get(str_item_phone_remove))
+    print(f'contact -> {contact}')
+    CONTACTS[contact[0].title()] = contact[1]
+    return f'Contact with name {contact[0].title()} and number {contact[1]} is added'
 
 
 def handler_show_all_contacts():
-    print(f'{CONTACTS}')
+    return CONTACTS
 
 
 def main():
-    pattern_add_conact = r'^[add]{3}\s{1}'
-    pattern_change_conact = r'^[change]{6}\s{1}'
-    pattern_get_contact = r'[phone]{5}\s{1}'
-
     while True:
         result = input("waiting command -> ...\n")
         if result.lower() in ("good bye", "close", "exit"):
             print(f'Good bye!')
             break
+        elif not result:
+            continue
 
-        elif result.lower() == "hello":
-            print(f"How can I help you?")
-
-        elif re.findall(pattern_add_conact, result):
-            # handler_add_contact(result)
-            input_error(handler_add_contact(result))
-
-        elif re.findall(pattern_change_conact, result):
-            handler_change_contact(result)
-
-        elif re.findall(pattern_get_contact, result):
-            handler_get_contact(result)
-
-        elif result.lower() == "show all":
-            handler_show_all_contacts()
+        else:
+            print(parser_command(result))
 
 
 if __name__ == "__main__":
