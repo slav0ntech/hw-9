724b69d02f29bf0d7bd5a10e69cff1aafd1266a0
slav0ntech <masqueradeit@gmail.com>
2023-08-16 10:22:31 +0300

    add def get_contact

--- main.py
+++ main.py
@@ -17,17 +17,21 @@ def input_error(inner):
 def parser_command(command):
     pattern_add_conact = r'^[aAdD]{3}\s{1}'
     pattern_change_conact = r'^[cChHaAnNgGeE]{6}\s{1}'
+    pattern_get_contact = r'[PpHhOoNnEe]{5}\s{1}'
 
     if re.findall(pattern_add_conact, command):
         x = [x.lower() for x in command.split(' ') if x.lower() != 'add']
         return handler_add_contact(x)
 
-    if re.findall(pattern_change_conact, command):
+    elif re.findall(pattern_change_conact, command):
         x = [x.lower() for x in command.split(' ') if x.lower() != 'change']
         return handler_change_contact(x)
 
+    elif re.findall(pattern_get_contact, command):
+        x = [x.lower() for x in command.split(' ') if x.lower() != 'phone']
+        return handler_get_contact(x)
+
     elif command.lower() == 'show all':
-        print(f'CONTACTS -> {CONTACTS}')
         return handler_show_all_contacts()
 
     else:
@@ -36,7 +40,6 @@ def parser_command(command):
 
 @input_error
 def handler_add_contact(contact: str):
-    print(f'contact -> {contact}')
     CONTACTS[contact[0].title()] = contact[1]
     return f'Contact with name {contact[0].title()} and number {contact[1]} has been added'
 
@@ -49,6 +52,11 @@ def handler_change_contact(contact: str):
         if key.lower() in dict_result:
             CONTACTS[key] = dict_result[key.lower()]
             return f'Contact with name {contact[0].title()} and number {contact[1]} has been changed'
+
+
+@input_error
+def handler_get_contact(contact: str):
+    return CONTACTS[contact[0].title()]
 
 
 def handler_show_all_contacts():
